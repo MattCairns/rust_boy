fa976816738c57370a9337703ef4b1f95a2834da
Matthew Cairns <matthew@openoceanrobotics.com>
2022-01-28 11:33:59 -0800

    Cleaned up code

---
+++ src/header.rs
@@ -0,0 +1,127 @@
+#[derive(Debug)]
+pub enum DestinationCode {
+    Japanese,
+    NonJapanese,
+    Unknown(u8),
+}
+
+#[derive(Debug)]
+pub enum CartridgeType {
+    RomOnly,
+    MBC1,
+    MBC2,
+    RomRam,
+    MMM01,
+    MBC3,
+    MBC5,
+    MBC6,
+    MBC7,
+    Unknown,
+}
+
+pub fn parse_cartridge_type(value: u8) -> CartridgeType {
+    match value {
+        0x00 => CartridgeType::RomOnly,
+        0x01..=0x03 => CartridgeType::MBC1,
+        0x05..=0x06 => CartridgeType::MBC2,
+        0x08..=0x09 => CartridgeType::RomRam,
+        0x0B..=0x0D => CartridgeType::MMM01,
+        0x0F..=0x13 => CartridgeType::MBC3,
+        0x19..=0x1E => CartridgeType::MBC5,
+        0x20 => CartridgeType::MBC6,
+        0x22 => CartridgeType::MBC7,
+        _ => CartridgeType::Unknown,
+    }
+}
+pub struct Header {
+    pub logo: Vec<u8>,
+    pub title: String,
+    pub new_licensee_code: u16,
+    pub old_licensee_code: u8,
+    pub sgb_flag: bool,
+    pub cartridge_type: CartridgeType,
+    pub rom_size: u8,
+    pub ram_size: u8,
+    pub destination_code: DestinationCode, // Should be enum
+    pub rom_version_num: u8,
+    pub header_checksum: u8,
+    pub global_checksum: u16,
+}
+
+impl Header {
+    pub fn new(data: &Vec<u8>) -> Self {
+        let logo = data[0x104..0x0133 + 0x1].to_vec();
+        let title = match std::str::from_utf8(&data[0x0134..0x0143 + 0x1]) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8: {}", e),
+        }
+        .to_string();
+
+        let destination_code = match data[0x014A] {
+            0x00 => DestinationCode::Japanese,
+            0x01 => DestinationCode::NonJapanese,
+            _ => DestinationCode::Unknown(data[0x014A]),
+        };
+
+        Header {
+            logo,
+            title,
+            new_licensee_code: 0, // TODO
+            old_licensee_code: 0, // TODO
+            sgb_flag: false,
+            cartridge_type: parse_cartridge_type(data[0x0147]),
+            rom_size: data[0x0148], // Make this an enum?
+            ram_size: data[0x0149], // Make this an enum?
+            destination_code,
+            rom_version_num: data[0x014C],
+            header_checksum: data[0x014D],
+            global_checksum: 0, // TODO
+        }
+    }
+
+    pub fn print_logo(&self) {
+        let mut logo_str = String::from("");
+        (0..8).for_each(|y| {
+            let mut i = ((y / 2) % 2) + (y / 4) * 24;
+            (0..12).for_each(|x| {
+                let nibble: u8 = if (y % 2) == 0 {
+                    self.logo[i] >> 4
+                } else {
+                    self.logo[i] & 0xF
+                };
+
+                (0..4).rev().for_each(|b| {
+                    if (nibble >> b) & 1 == 0 {
+                        logo_str.push(' ');
+                        logo_str.push(' ');
+                    } else {
+                        logo_str.push('▓');
+                        logo_str.push('▓');
+                    }
+                });
+
+                i += 2;
+            });
+
+            logo_str.push('\n');
+        });
+        println!("{}", logo_str);
+    }
+}
+
+impl std::fmt::Display for Header {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Title: {}\nDestination Code: {:?}\nCartridge Type: {:?}\nROM Size: {}\nRAM Size: {}\nROM Ver: {}\nHeader Checksum: {}\nLogo: {:X?}",
+            self.title,
+            self.destination_code,
+            self.cartridge_type,
+            self.rom_size,
+            self.ram_size,
+            self.rom_version_num,
+            self.header_checksum,
+            self.logo
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate ndarray;
 use ndarray::Array3;
 
 use rust_boy::cartridge::Cartridge;
-use rust_boy::cartridge::CartridgeHeader;
+use rust_boy::header::Header;
 use rust_boy::tile::Tile;
 // const CARTRIDGE_SIZE: usize = 0x200000;
 
@@ -19,53 +19,56 @@ fn main() {
     let mut screen = Array3::<u8>::zeros((SCREEN_WIDTH, SCREEN_HEIGHT, RGB_SZ));
     let mut ram: Vec<u8> = Vec::with_capacity(RAM_SZ);
 
-    let rom_path = "roms/pokemon.gb";
+    let rom_path = "roms/zelda.gb";
 
     let cartridge = Cartridge::load(rom_path); //.expect("Could not read ROM");
-    let header = CartridgeHeader::new(&cartridge.data);
+    let header = Header::new(&cartridge.data);
 
     let tile_test = Tile::new(header.logo[0..16].to_vec());
     // let tile_map_1 = cartridge.data[0x8000..0x87FF + 1].to_vec();
-    let mem_region_1 = 0x8000;
-    let mem_region_2 = 0x8800;
+    let mut mem_region_1 = 0x64010;
     let tile_sz = 16;
-    let tile_id_offset = 128;
+    // let tile_id_offset = 128;
 
-    (0..255).for_each(|id| {
-        let tile_addr = mem_region_1 + (id * tile_sz);
-        println!("{:X?}", tile_addr);
-        println!(
-            "{}",
-            Tile::new(cartridge.data[tile_addr..tile_addr + 16].to_vec())
-        );
-    });
-    // (0..255).for_each(|id| {
-    //
-    //p
-    //Sprite attrs at 0xFE00-0xFE9F
-    (0..40).for_each(|sprite| {
-        let loc = 0xFE00 + (sprite * 4) + 2;
-        let tile_loc: usize = cartridge.data[loc] as usize;
-        let tile_addr = mem_region_2 + (tile_loc * tile_sz);
+    /* (0..384).for_each(|id| {
+           // let tile_addr = mem_region_1 + (id * tile_sz);
+           // println!("{:X?}", tile_addr);
+           // println!("{:X?}", tile_addr + 15);
+           println!("{:X?}", mem_region_1);
+           println!(
+               "{}",
+               Tile::new(cartridge.data[mem_region_1..mem_region_1 + 16].to_vec())
+           );
+
+           mem_region_1 += 16;
+       });
+       println!(
+           "{}",
+           Tile::new(cartridge.data[0x640A0..0x640A0 + 16].to_vec())
+       );
+    */
+    /* (0..40).for_each(|sprite| {
+        // The OAM (Object Attribute Map) is empty until runtime
+        let y_pos = cartridge.data[0xFE00 + (sprite * 4) + 0] as usize;
+        let x_pos = cartridge.data[0xFE00 + (sprite * 4) + 1] as usize;
+        let tile_index = cartridge.data[0xFE00 + (sprite * 4) + 2] as usize;
+        let attrs = cartridge.data[0xFE00 + (sprite * 4) + 3] as usize;
+        println!("y_pos: {}", y_pos);
+        println!("x_pos: {}", x_pos);
+        println!("tile_index: {:?}", tile_index);
+        println!("attrs: {}", attrs);
+
+        let tile_addr = mem_region_1 + (tile_index * tile_sz);
         println!("{:X?}", tile_addr);
         println!("{:X?}", tile_addr + 15);
         println!(
             "{}",
             Tile::new(cartridge.data[tile_addr..tile_addr + 16].to_vec())
         );
-    });
-    // });
-    /* for i in (0..0x8FFF - 0x8000).step_by(16) {
-        println!("{}", Tile::new(tile_map_2[i..i + 16].to_vec()));
-    } */
-    let test: Vec<u8> = vec![
-        0xFF, 0x00, 0x7E, 0xFF, 0x85, 0x81, 0x89, 0x83, 0x93, 0x85, 0xA5, 0x8B, 0xC9, 0x97, 0x7E,
-        0xFF,
-    ];
-    println!("{}", Tile::new(test));
+    }); */
 
     println!("ROM Size: {}kb", cartridge.data.len() / 1024);
     println!("{}", header);
-    println!("{}", tile_test);
-    header.print_logo();
+    // println!("{}", tile_test);
+    // header.print_logo();
 }
