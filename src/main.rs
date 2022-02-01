bb69c99b7de9c9b926a38b010cca0b1a700b7e7e
Matthew Cairns <mattrcairns@gmail.com>
2022-01-31 22:55:07 -0800

    Add egui_sdl2_gl

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-extern crate sdl2;
-
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use sdl2::sys::KeyCode;
-use std::time::Duration;
+use egui::Checkbox;
+use egui_backend::sdl2::video::GLProfile;
+use egui_backend::{egui, gl, sdl2};
+use egui_backend::{sdl2::event::Event, DpiScaling, ShaderVersion};
+use std::time::Instant;
+// Alias the backend to something less mouthful
+use egui_sdl2_gl as egui_backend;
+use sdl2::video::SwapInterval;
 
 use rust_boy::cartridge::Cartridge;
 use rust_boy::cpu::Cpu;
@@ -22,38 +23,120 @@ fn main() {
     // SETUP SDL2
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(GLProfile::Core);
+    // For OpenGL ES Mesa driver 22.0.0+
+    // gl_attr.set_context_profile(GLProfile::GLES);
+
+    gl_attr.set_double_buffer(true);
+    gl_attr.set_multisample_samples(4);
 
     let window = video_subsystem
         .window("RustBoy", 500, 500)
-        .position_centered()
+        .opengl()
+        .resizable()
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    // Create a window context
+    let _ctx = window.gl_create_context().unwrap();
+    // Init egui stuff
+    let shader_ver = ShaderVersion::Default;
+    // On linux use GLES SL 100+, like so:
+    // let shader_ver = ShaderVersion::Adaptive;
+    let (mut painter, mut egui_state) =
+        egui_backend::with_sdl2(&window, shader_ver, DpiScaling::Custom(2.0));
+    let mut egui_ctx = egui::CtxRef::default();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
+    let mut test_str: String =
+        "A text box to write in. Cut, copy, paste commands are available.".to_owned();
+
+    let mut enable_vsync = false;
+    let mut quit = false;
+    let mut slider = 0.0;
+
+    let start_time = Instant::now();
     // GAME LOOP
     'running: loop {
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
+        if enable_vsync {
+            window
+                .subsystem()
+                .gl_set_swap_interval(SwapInterval::VSync)
+                .unwrap()
+        } else {
+            window
+                .subsystem()
+                .gl_set_swap_interval(SwapInterval::Immediate)
+                .unwrap()
+        }
+
+        egui_state.input.time = Some(start_time.elapsed().as_secs_f64());
+        egui_ctx.begin_frame(egui_state.input.take());
+
+        egui::CentralPanel::default().show(&egui_ctx, |ui| {
+            ui.label(" ");
+            ui.text_edit_multiline(&mut test_str);
+            ui.label(" ");
+            ui.add(egui::Slider::new(&mut slider, 0.0..=50.0).text("Slider"));
+            ui.label(" ");
+            ui.add(Checkbox::new(&mut enable_vsync, "Reduce CPU Usage?"));
+            ui.separator();
+            if ui.button("Quit?").clicked() {
+                quit = true;
+            }
+        });
+
+        let (egui_output, paint_cmds) = egui_ctx.end_frame();
+        // Process ouput
+        egui_state.process_output(&window, &egui_output);
+
+        // For default dpi scaling only, Update window when the size of resized window is very small (to avoid egui::CentralPanel distortions).
+        // if egui_ctx.used_size() != painter.screen_rect.size() {
+        //     println!("resized.");
+        //     let _size = egui_ctx.used_size();
+        //     let (w, h) = (_size.x as u32, _size.y as u32);
+        //     window.set_size(w, h).unwrap();
+        // }
+
+        let paint_jobs = egui_ctx.tessellate(paint_cmds);
+
+        // An example of how OpenGL can be used to draw custom stuff with egui
+        // overlaying it:
+        // First clear the background to something nice.
+        unsafe {
+            // Clear the screen to green
+            gl::ClearColor(0.3, 0.6, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        if !egui_output.needs_repaint {
+            if let Some(event) = event_pump.wait_event_timeout(5) {
+                match event {
+                    Event::Quit { .. } => break 'running,
+                    _ => {
+                        // Process input event
+                        egui_state.process_input(&window, event, &mut painter);
+                    }
+                }
+            }
+        } else {
+            painter.paint_jobs(None, paint_jobs, &egui_ctx.font_image());
+            window.gl_swap_window();
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. } => break 'running,
+                    _ => {
+                        // Process input event
+                        egui_state.process_input(&window, event, &mut painter);
+                    }
+                }
             }
         }
 
-        cpu.step();
-
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        if quit {
+            break;
+        }
     }
 
     /* let header = Header::new(&cartridge.data);
